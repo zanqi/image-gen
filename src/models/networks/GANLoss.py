5e069dcea91d6d83e8646cf65d5ef12add6edc27
Zanqi Liang <zanqiliang@fb.com>
2021-02-18 13:49:50 +0800

    GANLoss to avoid using tensor on cpu

--- src/models/networks/GANLoss.py
+++ src/models/networks/GANLoss.py
@@ -10,6 +10,8 @@ class GANLoss(nn.Module):
     def __init__(self, gan_mode):
         super(GANLoss, self).__init__()
         self.loss = nn.BCEWithLogitsLoss()
+        self.register_buffer('real_label', torch.tensor(1.0))
+        self.register_buffer('fake_label', torch.tensor(0.0))
 
     def __call__(self, prediction, target_is_real):
         target_tensor = self.get_target_tensor(prediction, target_is_real)
@@ -17,6 +19,6 @@ class GANLoss(nn.Module):
 
     def get_target_tensor(self, prediction, target_is_real):
         if target_is_real:
-            return torch.tensor(1.0).expand_as(prediction)
+            return self.real_label.expand_as(prediction)
         else:
-            return torch.tensor(0.0).expand_as(prediction)
+            return self.fake_label.expand_as(prediction)
