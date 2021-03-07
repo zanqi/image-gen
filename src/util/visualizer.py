10b80658da0ffba9a1f66e6baefe31cb1b291b7f
zanqi <zanqi.liang@gmail.com>
2021-03-07 15:55:23 +0800

    revert

--- src/util/visualizer.py
+++ src/util/visualizer.py
@@ -18,9 +18,9 @@ class Visualizer():
         fig = plt.figure(figsize=(16, 16))
         for i, (label, image) in enumerate(visuals.items()):
             image_numpy = util.tensor2im(image)
-            # img_path = os.path.join(
-            #     self.img_dir, 'epoch%.3d_%s.png' % (epoch, label))
-            # util.save_image(image_numpy, img_path)
+            img_path = os.path.join(
+                self.img_dir, 'epoch%.3d_%s.png' % (epoch, label))
+            util.save_image(image_numpy, img_path)
 
             axis = fig.add_subplot(2, 3, i+1)
             axis.set_title(label)
